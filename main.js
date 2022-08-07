4a4a2fa357db64bae247bcd471d59c2a9ccf4eca
reytiartur <reyti.artur@gmail.com>
2022-08-07 11:29:20 +0200

    Text related to progress added.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let hoursSet = document.querySelector("#set");
 let hoursReached = document.querySelector("#reached");
 let hoursLeft = document.querySelector("#left");
 
+let text = document.querySelector(".text");
+
 dailyInput.forEach(elem => elem.addEventListener("change", countDailyHours));
 hoursSet.addEventListener("change", showHoursLeft);
 window.addEventListener("change", showHoursLeft);
@@ -32,11 +34,15 @@ function setColor(elem, value) {
     value = +value;
     if(value == 0) {
         elem.style.boxShadow = "inset 0px 0px 28px 20px rgba(74,237,76,0.65)"
-    } else if(value > (hoursSet.value / 2)) {
-        elem.style.boxShadow = "inset 0px 0px 28px 20px rgba(237,72,66,0.65)"
-    } else if(value > (hoursSet.value / 4)) {
-        elem.style.boxShadow = "inset 0px 0px 28px 20px rgba(240,175,36,0.65)"
+        text.innerHTML = `Great! You achieved your goal for this week.`
     } else if(value < (hoursSet.value / 4)) {
         elem.style.boxShadow = "inset 0px 0px 28px 20px rgba(217,232,49,0.65)"
+        text.innerHTML = `${value} hours left. You can see the finish line.`
+    } else if(value > (hoursSet.value / 2)) {
+        elem.style.boxShadow = "inset 0px 0px 28px 20px rgba(237,72,66,0.65)"
+        text.innerHTML = `${value} hours left. You still have a long way ahead.`
+    } else if(value < (hoursSet.value / 2)) {
+        elem.style.boxShadow = "inset 0px 0px 28px 20px rgba(240,175,36,0.65)"
+        text.innerHTML = `${value} hours left. You have passed an equator.`
     }
 }
